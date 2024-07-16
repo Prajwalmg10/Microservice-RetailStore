cf0e0265ecaf787727305a80f1c827b67b22162b
Prajwalmg10 <121098209+Prajwalmg10@users.noreply.github.com>
2024-07-16 23:42:24 +0530

    add RetailStore microService

--- RetailStore/src/main/java/com/online/retailStore/Repository/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.online.retailStore.Repository;
-
-import com.online.retailStore.Models.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProductRepository extends JpaRepository<Product,Integer> {
-}
