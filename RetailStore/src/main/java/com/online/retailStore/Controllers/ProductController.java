443942b0ad79191f0a4017b001f87e5509574997
Prajwalmg10 <121098209+Prajwalmg10@users.noreply.github.com>
2024-07-24 23:05:18 +0530

    shopping service impl

--- RetailStore/src/main/java/com/online/retailStore/Controllers/ProductController.java
+++ RetailStore/src/main/java/com/online/retailStore/Controllers/ProductController.java
@@ -1,10 +1,29 @@
 package com.online.retailStore.Controllers;
 
+import com.online.retailStore.Pojos.Product;
+import com.online.retailStore.Services.ShoppingServices;
+import com.online.retailStore.UiResponse.DataException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/rest/api/shippingService")
-public class ProductController {
+public class ProductController extends AbstarctController {
+
+    @Autowired
+    private ShoppingServices shoppingServices;
+
+    @PostMapping("/product")
+    public ResponseEntity<?> createProduct(@RequestBody Product product){
+        try{
+         return    buildResponse(shoppingServices.createProduct(product));
+        }catch (DataException e){
+         return    buildError(e);
+        }
+    }
 
 }
--- RetailStore/src/main/java/com/online/retailStore/Services/Implementation/ShoppingServicesImpl.java
+++ RetailStore/src/main/java/com/online/retailStore/Services/Implementation/ShoppingServicesImpl.java
@@ -1,7 +1,13 @@
 package com.online.retailStore.Services.Implementation;
 
+import com.online.retailStore.Pojos.Product;
+import com.online.retailStore.UiResponse.DataException;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ShoppingServicesImpl implements com.online.retailStore.Services.ShoppingServices {
+    @Override
+    public Product createProduct(Product product) throws DataException {
+        return null;
+    }
 }
