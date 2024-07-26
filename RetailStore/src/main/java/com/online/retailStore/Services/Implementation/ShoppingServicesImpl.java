9ec1c9735b40d343e3ed0f499879002bff59b33e
Prajwalmg10 <121098209+Prajwalmg10@users.noreply.github.com>
2024-07-26 23:43:56 +0530

    RetailStore ClientTemplate

--- RetailStore/src/main/java/com/online/retailStore/Services/Implementation/ShoppingServicesImpl.java
+++ RetailStore/src/main/java/com/online/retailStore/Services/Implementation/ShoppingServicesImpl.java
@@ -2,12 +2,27 @@ package com.online.retailStore.Services.Implementation;
 
 import com.online.retailStore.Pojos.Product;
 import com.online.retailStore.UiResponse.DataException;
+import com.online.retailStore.Util.StringConstants;
+import com.online.retailStore.Util.WSClientTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ShoppingServicesImpl implements com.online.retailStore.Services.ShoppingServices {
+    @Autowired
+    private WSClientTemplate wsClientTemplate;
     @Override
     public Product createProduct(Product product) throws DataException {
+        try{
+            wsClientTemplate.invokeRESTService("",product, MediaType.APPLICATION_JSON, HttpMethod.POST);
+
+        }
+        catch (Exception e){
+            throw new DataException(StringConstants.EXCEPTION, e.getMessage(), HttpStatus.CONFLICT);
+        }
         return null;
     }
 }
--- RetailStore/src/main/java/com/online/retailStore/Services/ShoppingServices.java
+++ RetailStore/src/main/java/com/online/retailStore/Services/ShoppingServices.java
@@ -4,5 +4,6 @@ import com.online.retailStore.Pojos.Product;
 import com.online.retailStore.UiResponse.DataException;
 
 public interface ShoppingServices {
+
     Product createProduct(Product product) throws DataException;
 }
